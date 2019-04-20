3cae0bac75a3b8acd890f7771c5d0a006944049e
VyacheslavGoryunov <vyacheslav.goryunov@icloud.com>
2019-04-20 18:24:15 +0300

    Add phone switching Add phones sprites

--- Assets/Scripts/Shop/Phones/PhoneShop.cs
+++ Assets/Scripts/Shop/Phones/PhoneShop.cs
@@ -1,8 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhoneShop : MonoBehaviour
 {
     public PhoneDatabase Database;
+    public Image Preview;
+    public Text PriceLabel;
+    public Button LeftButton;
+    public Button RightButton;
+    public string PriceFormat = "{0} points";
+
+    public int Current
+    {
+        get => _current;
+        set
+        {
+            _current = value;
+            
+            if (_current >= Database.Phones.Length)
+            {
+                _current = 0;
+            } else if (_current < 0)
+            {
+                _current = Database.Phones.Length - 1;
+            }
+            
+            RefreshUi();
+        }
+    }
+
+    private int _current;
+
+    void Awake()
+    {
+        BindAll();
+        Current = 0;
+    }
+
+    protected void BindAll()
+    {
+        LeftButton.onClick.AddListener(Next);
+        RightButton.onClick.AddListener(Back);
+    }
+
+    public void Next()
+    {
+        Current ++;
+    }
+
+    public void Back()
+    {
+        Current--;
+    }
+
+    protected void RefreshUi()
+    {
+        if (_current > Database.Phones.Length)
+            return;
+
+        var phone = Database.Phones[_current];
+
+        Preview.sprite = phone.Picture;
+        PriceLabel.text = string.Format(PriceFormat, phone.Price);
+    }
 }
