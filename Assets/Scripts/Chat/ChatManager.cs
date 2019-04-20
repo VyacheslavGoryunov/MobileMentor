ac5ce87688c2cd1b21ad6121da4232e60a2f59d0
every <everywherealpha@gmail.com>
2019-04-21 00:52:51 +0300

    Add phone to the main scene Add chat scroll rect to phone foreground

---
+++ Assets/Scripts/Chat/ChatManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking.PlayerConnection;
+using UnityEngine.UI;
+
+public class ChatManager : MonoBehaviour
+{
+    public List<Message> Messages;
+    public RectTransform ChatBox;
+    public Message MessagePrefab;
+    private float _rowHeight;
+
+    private void Awake()
+    {
+        _rowHeight = MessagePrefab.GetComponent<RectTransform>().sizeDelta.y;
+    }
+
+    private void Start()
+    {
+        AddMessage("Макс Дорофеев", "Привет, это пробное сообщение!");
+        AddMessage("Вася Пупник", "Привет, это второе пробное сообщение!");
+        AddMessage("Макс Дорофеев", "Привет, это третье сообщение!");
+        AddMessage("Макс Дорофеев", "Привет, это третье сообщение!");
+        AddMessage("Макс Дорофеев", "Привет, это третье сообщение!");
+        AddMessage("Макс Дорофеев", "Привет, это третье сообщение!");
+        AddMessage("Макс Дорофеев", "Привет, это третье сообщение!");
+        AddMessage("Макс Дорофеев", "Привет, это третье сообщение!");
+        AddMessage("Макс Дорофеев", "Привет, это третье сообщение!");
+        AddMessage("Макс Дорофеев", "Привет, это третье сообщение!");
+    }
+
+    public void AddMessage(string senderName, string content)
+    {
+        var newMessage = Instantiate(MessagePrefab, ChatBox.transform);
+        newMessage.Name.text = senderName;
+        newMessage.Content.text = content;
+        Messages.Add(newMessage);
+        MeasureMessages();
+    }
+
+    public void MeasureMessages()
+    {
+        var height = _rowHeight * Messages.Count;
+        var size = ChatBox.sizeDelta;
+        ChatBox.sizeDelta = new Vector2(size.x, height);
+    }
+}
---
+++ Assets/Scripts/Chat/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class Message : MonoBehaviour
+{
+    public Text Name;
+    public Text Content;
+    public Image Avatar;
+}
